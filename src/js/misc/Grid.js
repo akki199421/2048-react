837859d28c44886a16ec2d609e970de3b424f06f
Akshay Gupta <akki199421@gmail.com>
2017-03-07 16:44:15 +0530

    Create Grid.js

---
+++ src/js/misc/Grid.js
@@ -0,0 +1,85 @@
+import React from 'react';
+
+import GameCntrl from './GameCntrl';
+import GridCell from '../components/GridCell'; 
+import GridTile  from '../components/GridTile';
+import Keyboard_Service from '../components/Keyboard_service';
+import GameHeading from '../components/GameHeading';
+import GameEnd from '../components/GameEnd';
+
+export default class Grid extends React.Component {
+	constructor(params){
+		super();
+		this.gridSize = params.gridSize;
+		this.GameCntrl = new GameCntrl(this.gridSize);
+		const {gridClass,score,gameWon,gameOver} = this.GameCntrl;
+		this.state = {
+			gridCls: gridClass,
+			score,
+			gameWon,
+			gameOver
+		};
+		
+		this.Key_Ser = new Keyboard_Service();
+		this.Key_Ser.startListen((key) => this.move(key));
+	}
+	setStateArg(callback){
+		const {gridClass,score,gameWon,gameOver} = this.GameCntrl;
+		this.setState({
+			gridCls: gridClass,
+			score,
+			gameWon,
+			gameOver
+		},callback);
+	}
+	move(key){ 
+		this.GameCntrl.move(key);
+		this.setStateArg();
+		//stop key service
+		if(this.state.gameWon || this.state.gameOver){
+			this.Key_Ser.stopListen();
+		}
+	}
+	continuefn(){
+		this.GameCntrl.gridClass.setWinnigs(999999);
+		this.setStateArg({gameWon : false},() => {this.Key_Ser.startListen((key) => this.move(key));});
+		this.GameCntrl.updateGameWon();
+	}
+	reInit(){
+		this.GameCntrl.clearGameState();
+		this.GameCntrl = new GameCntrl(this.gridSize);
+		this.setStateArg(() => {this.Key_Ser.startListen((key) => this.move(key));});
+	}
+	undoMove(){
+		this.GameCntrl.undoMove();
+		this.setStateArg();
+	}
+	render(){
+		const { grid,gridsize } = this.state.gridCls;
+		const { score,gameOver,gameWon } = this.state;
+		const cells = grid.map((tile,i) =>   <GridCell key={i} gridSize={gridsize}/>);
+		const tiles = grid.map((tile,i) => {
+			if(tile){
+				return <GridTile key={i} gridSize={gridsize} tile={tile}/>
+			}
+		});
+
+		return(
+			<div>
+				<GameHeading score={score} reInit={this.reInit.bind(this)} undoMove={this.undoMove.bind(this)}/>
+				<div class="game">
+					<div class="game-pos">
+						<div class="game-grid row">
+						{cells}
+						{tiles}
+						</div>
+						{gameWon || gameOver ? <GameEnd gameWon={gameWon} gameOver={gameOver} continuefn={this.continuefn.bind(this)}
+						reInit={this.reInit.bind(this)}/>: null}
+					</div>
+				</div>
+				
+				
+			</div>
+			);
+	}
+}
